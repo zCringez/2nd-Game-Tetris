44decbcd797fc682104b2395ecc3a6291b76278b
zCringez <118360627+zCringez@users.noreply.github.com>
2023-05-08 16:41:50 +0200

    Final-Final-Newfunctionadded-addedIntoDrawsection

--- index.js
+++ index.js
@@ -195,30 +195,43 @@ window.onload = () =>
              // Bewegt das Tetromino nach unten
                 tetromino.update(i => ++tetromino.y[i]);
         }
-        
         else {
-             // Aktualisiert die Anzeige von Spielstand und abgeschlossenen Zeilen
-            scoreLbl.innerText = score;
-            linesLbl.innerText = lines;
+    // Aktualisiert die Anzeige von Spielstand und abgeschlossenen Zeilen
+    scoreLbl.innerText = score;
+    linesLbl.innerText = lines;
 
-           // Erstellt ein neues, zufälliges Tetromino
-            tetromino = (({ x, y }, color) =>
-                new Tetromino([...x], [...y], color)
-            )(
-                TETROMINOES[Math.floor(Math.random() * (TETROMINOES.length - 1))],
-                Math.floor(Math.random() * (Tetromino.COLORS.length - 1))
-            );
+    // Erstellt ein neues, zufälliges Tetromino
+    tetromino = (({ x, y }, color) =>
+        new Tetromino([...x], [...y], color)
+    )(
+        TETROMINOES[Math.floor(Math.random() * (TETROMINOES.length - 1))],
+        Math.floor(Math.random() * (Tetromino.COLORS.length - 1))
+    );
 
-            tetromino.draw();
+    tetromino.draw();
+}
+// Pause-Logik
+let isPaused = false;
+
+function checkGameOver() {
+    for (let y = 0; y < MIN_VALID_ROW; y++) {
+        if (FIELD[y].some(block => block !== false)) {
+            alert("You have lost!");
+            reset();
+            return true;
         }
-        // Pause-Logik
-        let isPaused = false;
+    }
+    return false;
+}
 
 function draw() {
     if (!isPaused) {
         if (tetromino) {
             if (tetromino.collides(i => ({ x: tetromino.x[i], y: tetromino.y[i] + 1 }))) {
                 tetromino.merge();
+                if (checkGameOver()) {
+                    return;
+                }
                 tetromino = null;
             } else {
                 tetromino.update(i => ++tetromino.y[i]);
@@ -236,43 +249,43 @@ function draw() {
         }
     }
 
-        setTimeout(draw, delay);
-    }
-    //Steuerung
-    window.onkeydown = event => {
-        switch (event.key) {
-            case "a":
-                if (!tetromino.collides(i => ({ x: tetromino.x[i] - 1, y: tetromino.y[i] })))
-                    tetromino.update(i => --tetromino.x[i]);
-                break;
-            case "d":
-                if (!tetromino.collides(i => ({ x: tetromino.x[i] + 1, y: tetromino.y[i] })))
-                    tetromino.update(i => ++tetromino.x[i]);
-                break;
-            case "s":
-                delay = Tetromino.DELAY / Tetromino.DELAY_INCREASED;
-                break;
-            case " ":
-                tetromino.rotate();
-                break;
-            case "t":
-            case "T":
-                isPaused = !isPaused;
-                if (!isPaused) {
-                    draw();
-                }
-                break;
-        }
-    }
-    window.onkeyup = event => {
-        if (event.key === "s")
-            delay = Tetromino.DELAY;
-    }
-    
     setTimeout(draw, delay);
 }
 
+//Steuerung
+window.onkeydown = event => {
+    switch (event.key) {
+        case "a":
+            if (!tetromino.collides(i => ({ x: tetromino.x[i] - 1, y: tetromino.y[i] })))
+                tetromino.update(i => --tetromino.x[i]);
+            break;
+        case "d":
+            if (!tetromino.collides(i => ({ x: tetromino.x[i] + 1, y: tetromino.y[i] })))
+                tetromino.update(i => ++tetromino.x[i]);
+            break;
+        case "s":
+            delay = Tetromino.DELAY / Tetromino.DELAY_INCREASED;
+            break;
+        case " ":
+            tetromino.rotate();
+            break;
+        case "t":
+        case "T":
+            isPaused = !isPaused;
+            if (!isPaused) {
+                draw();
+            }
+            break;
+    }
+}
+window.onkeyup = event => {
+    if (event.key === "s")
+        delay = Tetromino.DELAY;
 }
 
-        
-    
+setTimeout(draw, delay);
+
+}
+ }
+
+   
